73d20536b479ec0739d1aac25ba82870839dd94f
25bcsi56_satya ranjan das <satyaranjanchinu@gmail.com>
2026-05-26 16:13:40 +0530

    Add files via upload

---
+++ day 1/my_intro.java
@@ -0,0 +1,13 @@
+package javaproject;
+
+public class my_intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Name: is Satya Rananjan Das.");
+		System.out.println("college: silicon university");
+		System.out.println("department: cse ");
+		System.out.println("Hobbies: Gameing,sleping");
+	}
+
+}
---
+++ day 1/resturant_bill.java
@@ -0,0 +1,30 @@
+package javaproject;
+
+public class resturant_bill {
+
+	    public static void main(String args[])
+	    {
+	        System.out.println("restaurant bill");
+
+	        System.out.println("restaurant name-ABC hotel");
+
+	        System.out.println("3 food items");
+	     
+
+	        System.out.println("\n********** hotel bill *********");
+
+	        System.out.println("Restaurant name : ABC hotel");
+
+	        System.out.println("Food Items:");
+
+	        System.out.println("1.Fried rice : 120");
+	        System.out.println("2.Noodles : 100");
+	        System.out.println("3.Icecream : 100");
+
+	        System.out.println("-----------------------------------");
+
+	        System.out.println("total amount: 320");
+
+	        System.out.println("************************************");
+	    }
+}
